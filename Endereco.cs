3bab66d7593c8dc0e398bfef09286bb88171f403
rafaelfaculdadeuna-hash <rafaelfaculdadeuna@gmail.com>
2026-02-15 14:25:20 -0300

    Add files via upload
    entrega 01

---
+++ Endereco.cs
@@ -0,0 +1,17 @@
+using System.Text.Json.Serialization;
+public class Endereco
+{
+[JsonPropertyName("cep")]
+public string Cep { get; set; }
+[JsonPropertyName("logradouro")]
+public string Logradouro { get; set; }
+[JsonPropertyName("complemento")]
+public string Complemento { get; set; }
+[JsonPropertyName("bairro")]
+public string Bairro { get; set; }
+[JsonPropertyName("localidade")]
+public string Localidade { get; set; }
+[JsonPropertyName("uf")]
+public string Uf { get; set; }
+[JsonPropertyName("ibge")]
+public string Ibge { get; set; }
